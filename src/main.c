0624e1e8cb8e8cba2d9c499f19e7a62d1a2b9b2b
Mustafa Siddiqui <mustafa.eins@gmail.com>
2021-07-11 02:11:10 -0700

    update to run shell loop only

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /**
  *  @file   main.c
  *  @brief  The driver code for the shell program.
+ *          => msh -> My Shell <=
  *  @author Mustafa Siddiqui
  *  @date   07/02/21
  */
@@ -9,25 +10,11 @@
 #include <stdlib.h>
 #include <assert.h>
 // -- //
-#include "linked_list.h"
 #include "msh.h"
 
-/*
-    Helpful macro to declare and set value for variable given
-    the datatype. ** uses pointers, not basic variables **
-    => requires assert.h to check memory allocation <=
-*/
-#define SET(val, var, type) type* var = _ALLOC_MEM(type);   \
-                            assert(var);                    \
-                            *var = val
-
 int main(void) {
-    char** tokens = parseLine(readLine());
-    int i = 0;
-    while (tokens[i] != NULL) {
-        printf("%s\n", tokens[i]);
-        i++;
-    }
+    // run shell loop
+    mshLoop();
 
     return 0;
 }
